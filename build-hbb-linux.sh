b4da1efcc967a6da8cd35edff3283518bc4e3c4b
jsmall-nvidia <jsmall@nvidia.com>
2023-01-04 12:31:14 -0500

    Use centos premake.

--- build-hbb-linux.sh
+++ build-hbb-linux.sh
@@ -2,7 +2,10 @@ OS=linux
 PLATFORM=$1
 CONFIGURATION=$2
 
-PREMAKE=external/slang-binaries/premake/premake-5.0.0-alpha16/bin/linux-64/premake5
+CURL_CA_BUNDLE=external/slang-binaries/certificate/linux/ca-bundle.crt
+export CURL_CA_BUNDLE
+
+PREMAKE=external/slang-binaries/premake/premake-5.0.0-alpha16/bin/centos-7-x64/premake5
 chmod u+x ${PREMAKE}
 ${PREMAKE} gmake --deps=true --no-progress=true
 
